131f7270de2cc3dbbff88c297cd44929b06a0b54
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2024-05-02 14:01:23 +0300

    finished affirmations app

---
+++ AfirmationsApp/app/src/main/java/com/github/vikie1/afirmationsapp/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.github.vikie1.afirmationsapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
